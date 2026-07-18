e468b71f95c44857f1b34d9ef533feeb842ddcf5
Ashish Kumar <ashishkumar@macbook.local>
2026-07-18 15:12:34 +0530

    trigger pipeline

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ resource "kubernetes_horizontal_pod_autoscaler_v2" "nginx_hpa" {
 
   spec {
     min_replicas = 1
-    max_replicas = 5
+    max_replicas = 3
 
     scale_target_ref {
       api_version = "apps/v1"
