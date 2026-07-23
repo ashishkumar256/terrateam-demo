d06d0df219fbf11ae080ee3c33e3450a88bfb00c
ashishkumar256 <70199915+ashishkumar256@users.noreply.github.com>
2026-07-23 15:10:33 +0530

    Change Terraform backend to S3 with LocalStack
    Updated Terraform backend configuration to use S3 with LocalStack.

--- backend.tf
+++ backend.tf
@@ -1,13 +1,16 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "karpenter-convert"
-#     key    = "tfstate"
-#     region = "ap-south-1"
-#   }
-# }
-
 terraform {
-  backend "local" {
-    path = "/opt/tfstate/${terraform.workspace}/poc.tfstate"
+  backend "s3" {
+    bucket                      = "tofu-backend"
+    key                         = "${terraform.workspace}/poc.tfstate"
+    region                      = "ap-south-1"
+    
+    # Directs traffic to your LocalStack instance
+    endpoint                    = "http://localhost:31566" 
+    
+    # Skips AWS-specific verification steps that break locally
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    use_path_style             = true 
   }
 }
