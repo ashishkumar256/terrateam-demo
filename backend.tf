da5f0355f64c7ec9a57a862625bdc3a571756550
Ashish Kumar <ashishkumar@macbook.local>
2026-07-25 11:58:18 +0530

    debug

--- backend.tf
+++ backend.tf
@@ -5,8 +5,7 @@ terraform {
     region                      = "ap-south-1"
     
     # Directs traffic to your LocalStack instance
-    endpoint                    = "https://9cb85f3b38ab-10-244-8-159-31566.spca.r.killercoda.com" 
-
+    endpoint                    = "https://4b58d12a19a2-10-244-8-154-31566.spca.r.killercoda.com"
     # Skips AWS-specific verification steps that break locally
     skip_credentials_validation = true
     skip_metadata_api_check     = true
