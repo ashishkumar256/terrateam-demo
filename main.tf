90e06d7981ecc0f02f693c95e860802e10ffba1b
ashishkumar256 <70199915+ashishkumar256@users.noreply.github.com>
2026-08-01 02:29:40 +0530

    Delete temporary Kubernetes namespace resource
    Removed temporary Kubernetes namespace resource.

--- main.tf
+++ main.tf
@@ -8,12 +8,6 @@ resource "kubernetes_namespace" "nginx_ns" {
   }
 }
 
-resource "kubernetes_namespace" "tmp_ns" {
-  metadata {
-    name = "tmp"
-  }
-}
-
 # ----------------------------
 # NGINX Deployment
 # ----------------------------
